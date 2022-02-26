c7348ee6c072d58f8e7f9a88a97ca93929e840d2
code-diggers-369 <codediggers007@gmail.com>
2022-02-26 12:22:07 +0530

    23 - Basic Async Storage In React Native

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,78 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import React, {useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Home() {
+  const [inputBoxValue, setInputBoxValue] = useState('');
+
+  const [storageDataList, setStorageDataList] = useState('');
+
+  const addItemToList = async () => {
+    try {
+      await AsyncStorage.setItem('itemList', inputBoxValue);
+      setInputBoxValue('');
+
+      await getItemList();
+
+      alert('Data Is Added');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getItemList = async () => {
+    try {
+      const data = await AsyncStorage.getItem('itemList');
+      setStorageDataList(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text
-        style={{
-          fontSize: 50,
-          fontFamily: 'SupermercadoOne-Regular',
-        }}>
-        Home
-      </Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.inputBox}
+        value={inputBoxValue}
+        placeholder="Enter Data"
+        onChangeText={value => setInputBoxValue(value)}
+      />
+
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => addItemToList()}>
+        <Text style={{color: '#fff'}}>Add</Text>
+      </TouchableOpacity>
+
+      <Text>Your Data is {storageDataList}</Text>
     </View>
   );
 }
+
+const {width} = Dimensions.get('screen');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputBox: {
+    borderWidth: 2,
+    borderColor: 'black',
+    marginVertical: 10,
+    marginHorizontal: 8,
+  },
+  addButton: {
+    width: width - 20,
+    backgroundColor: 'blue',
+    marginHorizontal: 10,
+    alignItems: 'center',
+    padding: 10,
+  },
+});
