6ec92f62e6174f654c21f0c7d9e7d19ccecaea95
code-diggers-369 <codediggers007@gmail.com>
2022-07-23 08:36:36 +0530

    35 - Check Netinfo State In React Native update

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useNetInfo} from '@react-native-community/netinfo';
 
 //
@@ -8,14 +8,22 @@ import StackNavigator from './src/navigation/StackNavigator/StackNavigator';
 import DisconnectPage from './src/components/DisconnectPage';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  //
   const net = useNetInfo();
 
   console.log(net);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 100);
+
   // return <>{net.isConnected ? <StackNavigator /> : <DisconnectPage />}</>;
   return (
     <>
       <StackNavigator />
-      {!net.isConnected ? <DisconnectPage /> : null}
+      {!net.isConnected && !isLoading ? <DisconnectPage /> : null}
     </>
   );
 }
