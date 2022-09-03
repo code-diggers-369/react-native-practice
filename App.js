d41304e3e192aa518df8a09910aef56a25622ff4
code-diggers-369 <codediggers007@gmail.com>
2022-09-03 21:52:28 +0530

    37 - Play Video In React Native

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
-import React, {useState} from 'react';
-import {useNetInfo} from '@react-native-community/netinfo';
+import React from 'react';
 
 //
 import StackNavigator from './src/navigation/StackNavigator/StackNavigator';
 
-//
-import DisconnectPage from './src/components/DisconnectPage';
-
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  //
-  const net = useNetInfo();
-
-  console.log(net);
-
-  setTimeout(() => {
-    setIsLoading(false);
-  }, 100);
-
-  // return <>{net.isConnected ? <StackNavigator /> : <DisconnectPage />}</>;
   return (
     <>
       <StackNavigator />
-      {!net.isConnected && !isLoading ? <DisconnectPage /> : null}
     </>
   );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,27 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-import {useNetInfo} from '@react-native-community/netinfo';
+import Video from 'react-native-video';
 
 export default function Home() {
-  const net = useNetInfo();
-
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Type = {net.type}</Text>
-      <Text>isConnected = {net.isConnected ? 'connect' : 'disconnect'}</Text>
+    <View style={{flex: 1}}>
+      <Video
+        source={{
+          uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+        }}
+        style={styles.backgroundVideo}
+      />
     </View>
   );
 }
+
+// Later on in your styles..
+var styles = StyleSheet.create({
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
