ecfcb8d9fcd1d2f4a17d8ee0b33c3284eb7964e3
code-diggers-369 <codediggers007@gmail.com>
2022-09-17 14:29:30 +0530

    38 - Add Video Controller In React Native

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,31 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import Video from 'react-native-video';
+import VideoPlayer from 'react-native-video-controls';
+import {useNavigation} from '@react-navigation/native';
 
 export default function Home() {
+  const navigate = useNavigation();
   return (
     <View style={{flex: 1}}>
-      <Video
+      {/* <Video
         source={{
           uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
         }}
         style={styles.backgroundVideo}
+      /> */}
+      <VideoPlayer
+        source={{
+          uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+        }}
+        navigator={navigate}
+        controlTimeout={2000}
+        onPlay={() => {
+          console.log('video is playing');
+        }}
+        onPause={() => {
+          console.log('video is pause');
+        }}
       />
     </View>
   );
