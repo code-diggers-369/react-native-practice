2766bb3cea8be1183e9058c22bb8c05da5fa1e34
code-diggers-369 <codediggers007@gmail.com>
2023-01-08 15:12:29 +0530

    43 - Download Qr Code In React Native

--- src/screens/Generator.js
+++ src/screens/Generator.js
@@ -1,9 +1,35 @@
 import {View, Text, Button, TextInput} from 'react-native';
 import React, {useState} from 'react';
 import Qrcode from 'react-native-qrcode-svg';
+import RNFetchBlob from 'rn-fetch-blob';
 
 export default function Home() {
   const [qrCodeText, setQrCodeText] = useState('');
+
+  const [qrCodeRef, setQrCodeRef] = useState(null);
+
+  const downloadQrcode = () => {
+    try {
+      // alert('hello');
+      qrCodeRef.toDataURL(async data => {
+        // console.log(data);
+        const path =
+          RNFetchBlob.fs.dirs.DownloadDir +
+          `/${qrCodeText
+            .replace('http', '')
+            .replace('://', 'a')
+            .replace('.', '_')
+            .slice(0, 20)}.png`;
+
+        await RNFetchBlob.fs.writeFile(path, data, 'base64');
+
+        alert('Download Successfully');
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <View style={{flex: 1, backgroundColor: 'black'}}>
       <View style={{alignItems: 'center', marginTop: 10}}>
@@ -25,8 +51,11 @@ export default function Home() {
           value={qrCodeText ? qrCodeText : 'https://www.google.com/'}
           size={300}
           logo={require('../../assets/img/logo.png')}
+          getRef={ref => setQrCodeRef(ref)}
         />
       </View>
+
+      <Button title="Download Qrcode" onPress={() => downloadQrcode()} />
     </View>
   );
 }
