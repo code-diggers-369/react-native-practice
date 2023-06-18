6efade9e744fb26f05f2b2d5d450f615554500b5
code-diggers-369 <codediggers007@gmail.com>
2023-06-18 13:18:41 +0530

    45 - Add Skeleton Loader In React Native

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,33 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Dimensions } from 'react-native';
 import React, { useState } from 'react';
+import ContentLoader, { Rect, Circle } from 'react-content-loader/native'
 
 export default function Home() {
+  const { height, width } = Dimensions.get('screen')
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: 'black',
         justifyContent: 'center',
         alignItems: 'center',
-      }}>
-      <Text style={{ fontSize: 50, color: "white" }}>Hello World</Text>
+        backgroundColor: "white"
+      }}
+    >
+      <ContentLoader
+        speed={2}
+        width={400}
+        height={160}
+        viewBox="0 0 400 160"
+        backgroundColor="#d9d9d9"
+        foregroundColor="#ededed"
+      >
+        <Rect x="50" y="6" rx="4" ry="4" width="343" height="38" />
+        <Rect x="8" y="6" rx="4" ry="4" width="35" height="38" />
+        <Rect x="50" y="55" rx="4" ry="4" width="343" height="38" />
+        <Rect x="8" y="55" rx="4" ry="4" width="35" height="38" />
+        <Rect x="50" y="104" rx="4" ry="4" width="343" height="38" />
+        <Rect x="8" y="104" rx="4" ry="4" width="35" height="38" />
+      </ContentLoader>
     </View>
   );
 }
